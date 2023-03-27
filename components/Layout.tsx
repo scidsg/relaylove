3bf20b5290fc9a3193cc82096c861d7fe0f74ece
wlaith <laethwazani@gmail.com>
2023-03-27 13:10:44 +0300

    feat: added card as compononet

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,7 @@ import grid from '@/styles/grid.module.css';
 import heading from '@/styles/headings.module.css';
 import list from '@/styles/List.module.css';
 import Link from 'next/link'
+import Card from './Card'
 
 
 
@@ -31,11 +32,14 @@ const Layout = ({ children }: LayoutProps) => {
       </Head>
 
       <Nav></Nav>
+
+      {/* main content */}
+
       <div className='bg-darkPurple'>
         <div className={grid.gridTwelve}>
         <Snowflake></Snowflake>
 
-        <div className="p-16 md:p-0 md:col-end-8 md:col-start-5 md:row-start-1 mt-5 md:pr-6 lg:p-0">
+        <div className="p-10 pb-0 md:p-0 md:col-end-8 md:col-start-5 md:row-start-1 mt-5 md:pr-6 lg:p-0">
           <h2 className={heading.headingTwo}>شارك حرية الانترنت</h2>
           <ul className={list.List}>
             <li className={list.Items}>اضغط الزر</li>
@@ -44,7 +48,7 @@ const Layout = ({ children }: LayoutProps) => {
           </ul>
         </div>
 
-        <div className="p-16 md:p-0 text-right w-full md:col-start-13 md:col-end-1 md:mr-0 md:row-start-2 lg:col-end-4 lg:col-start-1 lg:row-start-1 mt-5">
+        <div className="p-10  md:p-0 text-right w-full md:col-start-13 md:col-end-1 md:mr-0 md:row-start-2 lg:col-end-4 lg:col-start-1 lg:row-start-1">
           <p className='fontSize-5xl'>
           يمكنك مساعدة الأشخاص على تجاوز الرقابة من خلال تمكين وكيل سنوفليك على يمينك. بمجرد أن تتحول أيقونة ندفة الثلج إلى اللون الأخضر، فهذا يعني أن المستخدم في بلد خاضع للرقابة يتصل من خلال وكيلك للوصول إلى الإنترنت دون قيود.
           </p>
@@ -55,6 +59,33 @@ const Layout = ({ children }: LayoutProps) => {
 
         </div>
       </div>
+
+
+      {/* cards */}
+      <Card imagePath='/images/map.svg'>
+        <h2 className={heading.headingTwo}>شبكة تور</h2>
+        <p>
+           لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار  النشوة وتمجيد الألم نشأت بالفعل، وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور بالسعادة، ولكن بفضل هؤلاء الأشخاص الذين لا يدركون بأن السعادة لا بد أن نستشعرها بصورة أكثر عقلانية ومنطقية فيعرضهم هذا لمواجهة الظروف الأليمة، وأكرر بأنه لا يوجد من يرغب في الحب ونيل المنال ويتلذذ بالآلام، الألم هو الألم ولكن نتيجة لظروف ما قد تكمن السعاده فيما نتحمله من كد وأسي.
+        </p>
+
+      </Card>
+
+      <Card>
+        <h2 className={heading.headingTwo}>برررر</h2>
+        <p>
+           لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار  النشوة وتمجيد الألم نشأت بالفعل، وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور بالسعادة، ولكن بفضل هؤلاء الأشخاص الذين لا يدركون بأن السعادة لا بد أن نستشعرها بصورة أكثر عقلانية ومنطقية فيعرضهم هذا لمواجهة الظروف الأليمة، وأكرر بأنه لا يوجد من يرغب في الحب ونيل المنال ويتلذذ بالآلام، الألم هو الألم ولكن نتيجة لظروف ما قد تكمن السعاده فيما نتحمله من كد وأسي.
+        </p>
+
+      </Card>
+
+      <Card>
+      <h2 className={heading.headingTwo}>بسبس</h2>
+        <p>
+           لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار  النشوة وتمجيد الألم نشأت بالفعل، وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور بالسعادة، ولكن بفضل هؤلاء الأشخاص الذين لا يدركون بأن السعادة لا بد أن نستشعرها بصورة أكثر عقلانية ومنطقية فيعرضهم هذا لمواجهة الظروف الأليمة، وأكرر بأنه لا يوجد من يرغب في الحب ونيل المنال ويتلذذ بالآلام، الألم هو الألم ولكن نتيجة لظروف ما قد تكمن السعاده فيما نتحمله من كد وأسي.
+        </p>
+
+      </Card>
+
       
     
     </>
--- components/card.tsx
+++ components/card.tsx
@@ -1,15 +1,20 @@
-import { Children } from "react";
+import Image from 'next/image';
 
-const Card = ({Children}) => {
-    return (
-      <>
-        <div className="bg-lightPurple">
-            {Children}
-        </div>
+type Props = {
+  children: React.ReactNode;
+  imagePath?: string;
+};
+
+const Card = ({ children, imagePath }: Props) => {
+  return (
+    <div className='bg-lightPurple p-5 md:p-20 m-7 sm:m-7 md:m-20 rounded-lg'>
+
+      <div>{children}</div>
+    
+      {imagePath && <Image className='w-full p-0 pt-3 sm:p-5 md:p-10 lg:p-70' src={imagePath} alt="Card Image" width={100} height={100} />}
       
-      </>
-    );
-  };
-  
-  export default Card;
-  
+    </div>
+  );
+};
+
+export default Card;
